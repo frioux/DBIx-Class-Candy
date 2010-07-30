8347385bb1218183e2702dec95610958ed4f9f49
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2010-07-29 19:06:27 -0500

    Fix stupid errors in ::Exports

--- lib/DBIx/Class/Candy/Exports.pm
+++ lib/DBIx/Class/Candy/Exports.pm
@@ -8,8 +8,8 @@ use warnings;
 our %methods;
 our %aliases;
 
-sub export_methods        { $methods{scalar caller(1)} = $_[1] }
-sub export_method_aliases { $aliases{scalar caller(1)} = $_[1] }
+sub export_methods        { $methods{scalar caller(0)} = $_[0] }
+sub export_method_aliases { $aliases{scalar caller(0)} = $_[0] }
 
 use Sub::Exporter -setup => {
    exports => [ qw(export_methods export_method_aliases) ],
