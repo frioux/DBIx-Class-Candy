1f63327dfcaf1029b221f874cc0b447c89f5542a
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2011-08-30 22:21:23 -0500

    test weird case

--- t/lib/IRC/Schema/Result/Network.pm
+++ t/lib/IRC/Schema/Result/Network.pm
@@ -2,14 +2,14 @@ package IRC::Schema::Result::Network;
 
 use IRC::Schema::Result;
 
+__PACKAGE__->add_columns( name => {
+   data_type => 'varchar',
+   size      => 100,
+});
+
 primary_column id => {
    data_type => 'int',
    is_auto_increment => 1,
-};
-
-column name => {
-   data_type => 'varchar',
-   size      => 100,
 };
 
 unique_constraint [qw( name )];
